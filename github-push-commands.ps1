f135f9e9de20dbb6c0a512f1ca649741b083f1ae
dadsdoingdesign <github@dadsdoingdesign.com>
2025-03-08 10:18:14 -0500

    Add website files for Vercel deployment

---
+++ github-push-commands.ps1
@@ -0,0 +1,5 @@
+# Add the GitHub remote repository
+& "C:\Program Files\Git\bin\git.exe" remote add origin https://github.com/DadsDoingDesign/martinez-campaign-images.git
+
+# Push the main branch to GitHub
+& "C:\Program Files\Git\bin\git.exe" push -u origin master
